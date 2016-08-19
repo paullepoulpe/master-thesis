9d233fe56ee508ae438d197540aae9a9e396f9d6
Damien Engels <engelsdamien@gmail.com>
2016-08-19 12:39:29 -0700

    Add info to install tools

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+## Document things needed to compile this
+brew install pandoc           # Markdonw to latex
+brew cask install mactex      # Latex compilation
+npm i markdown-spellcheck -g  # Spell check
+brew install fswatch          # Auto reload
